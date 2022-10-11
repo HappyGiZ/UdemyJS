78ca53a4d10bdd4839d0cd2247816cc595798e2e
Igor <gizmo0787@gmail.com>
2022-10-11 18:56:00 +0300

    less than 21

--- lesson_practice_5_6/js/script.js
+++ lesson_practice_5_6/js/script.js
@@ -1,18 +1,3 @@
-//!
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -27,34 +12,22 @@ document.addEventListener('DOMContentLoaded', () => {
         ]
     };
 
-    // 1
+    const adv = document.querySelectorAll('.promo__adv img'),
+        genre = document.querySelector('.promo__genre'),
+        poster = document.querySelector('.promo__bg'),
+        buttonSubmin = document.querySelector('button'),              // кнопка "подтвердить"
+        textInput = document.querySelector('.adding__input'),
+        checkbox = document.querySelector('input[type=checkbox]');
 
-    const adv = document.querySelectorAll('.promo__adv img');
-
-    adv.forEach(elem => {
+    adv.forEach(elem => {       // удаление рекламных баннеров
         elem.remove();
     });
 
-    // 2
-
-    const genre = document.querySelector('.promo__genre');
 
     genre.textContent = 'ДРАМА';
 
-    // 3
-
-    const poster = document.querySelector('.promo__bg');
-
     poster.style.backgroundImage = 'url("img/bg.jpg")';
-    // poster.style.background = 'url(img/bg.jpg) center center/cover no-repeat';
 
-    // 4 + 5
-
-    // const list = document.querySelectorAll('.promo__interactive-item'); // список
-
-    // for (let k = 0; k < movieDB.movies.length; k++) {
-    //     list[k].textContent = `${k + 1} - ${movieDB.movies.sort()[k]}`;
-    // }
 
     function newMovieList() {
 
@@ -94,35 +67,38 @@ document.addEventListener('DOMContentLoaded', () => {
     5) Фильмы должны быть отсортированы по алфавиту */
 
     // 1
-    const buttonSubmin = document.querySelector('button'),              // кнопка "подтвердить"
-        textInput = document.querySelector('.adding__input'),
-        checkbox = document.querySelector('input[type=checkbox]');
+
 
     buttonSubmin.addEventListener('click', (e) => {
         e.preventDefault();
-        
-        movieDB.movies.push(textInput.value);                           // добавление нового фильма в базу
 
-        newMovieList();                                                 // вставка нового фильма на страницу
+        let newFilm = '';
+
+        if (textInput.value.length > 21) {
+            newFilm = `${textInput.value.slice(0, 21)}...`;
+        } else {
+            newFilm = textInput.value;
+        }
+
+        movieDB.movies.push(newFilm);   // добавление нового фильма в базу
+
+        newMovieList();                 // вставка нового фильма на страницу
 
         if (checkbox.checked) {
-            console.log(`Добавляем любимый фильм ${textInput.value.toUpperCase()}`);
+            console.log(`Добавляем любимый фильм ${newFilm.toUpperCase()}`);
         }
 
     });
 
-    // 2
 
 
-    // 3
-    const trash = document.querySelectorAll('.delete');
+
+
+    const trash = document.querySelectorAll('.promo__menu-item promo__menu-item_active');
     trash.forEach(element => {
         element.addEventListener('click', () => {
             console.log(`поймал`);
         });
     });
-
-
-    // 4
 
 });
