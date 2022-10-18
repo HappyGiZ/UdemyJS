df80d2d2a1f294f5da6909b491d00495ead71956
Igor Golovan <gizmo0787@gmail.com>
2022-10-18 15:00:06 +0300

    modal window

---
+++ js/temp.js
@@ -0,0 +1,11 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+    const box = document.querySelector('.box');
+
+    box.addEventListener('touchstart', (e) => {
+        e.preventDefault();
+
+        console.log('start');
+    })
+})
