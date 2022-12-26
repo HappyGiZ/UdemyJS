3d7c39e7cff3bdf0e7d4a887a04d603326425c56
Igor <gizmo0787@gmail.com>
2022-12-26 21:27:45 +0300

    temp

--- temp/temp.js
+++ temp/temp.js
@@ -1,22 +1,13 @@
 'use strict';
 
-try {
-  console.log('All OK');
-  console.log(a);
-} catch (error) {
-  console.log(`Some troubles! ${error}`);         // Сообщение об ошибке
-  console.log(`Error name: ${error.name}`);       // Название ошибки
-  console.log(`Error message: ${error.message}`); // Сообщение ошибки
-  console.log(`Error stack: ${error.stack}`);     // Что привело к ошибке
-} finally {
-  console.log('Anyway OK');
-}
-console.log('Still OK');
+console.log(1);
 
-// All OK
-// Some troubles! ReferenceError: a is not defined
-// Error name: ReferenceError
-// Error message: a is not defined
-// Error stack: ReferenceError: a is not defined
-// Anyway OK
-// Still OK
+setTimeout(() => {
+  console.log(2);
+}, 2000);
+
+setTimeout(() => {
+  console.log(3);
+}, 4000);
+
+console.log(4);
